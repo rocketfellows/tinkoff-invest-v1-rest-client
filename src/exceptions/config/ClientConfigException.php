d6b28e7d9f1827970fd2310756c34325f3ccea19
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-02 00:53:55 +0400

    Added exception.

---
+++ src/exceptions/config/ClientConfigException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1HttpClient\exceptions\config;
+
+use rocketfellows\TinkoffInvestV1HttpClient\ClientConfig;
+
+abstract class ClientConfigException extends ClientConfig
+{
+}
