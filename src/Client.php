bc085c085fa1f4412b935b33c6b85b2a3f7b747f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-02 01:01:25 +0400

    Added injections.

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,16 @@
 
 namespace rocketfellows\TinkoffInvestV1HttpClient;
 
+use GuzzleHttp\Client as HttpClient;
+
 class Client
 {
+    private $config;
+    private $httpClient;
+
+    public function __construct(ClientConfig $config, HttpClient $httpClient)
+    {
+        $this->config = $config;
+        $this->httpClient = $httpClient;
+    }
 }
