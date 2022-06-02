2800f924ec078b747b38f14121e27796937f5603
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-03 00:20:27 +0400

    Added exception.

---
+++ src/exceptions/request/RequestException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1RestClient\exceptions\request;
+
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\ClientException;
+
+abstract class RequestException extends ClientException
+{
+}
