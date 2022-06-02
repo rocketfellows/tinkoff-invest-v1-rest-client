992787040f49186ce0287a84c5e9944a26ebf644
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-03 00:51:11 +0400

    Added VO.

---
+++ src/exceptions/request/BadResponseData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1RestClient\exceptions\request;
+
+class BadResponseData
+{
+    private $code;
+    private $message;
+    private $description;
+
+    public function __construct(
+        ?int $code = null,
+        ?string $message = null,
+        ?string $description = null,
+    ) {
+        $this->code = $code;
+        $this->message = $message;
+        $this->description = $description;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+}
