6d082038ca4b694d3d1745df37e547da5631225e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-04 00:32:52 +0400

    Added assertions.

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -3,10 +3,16 @@
 namespace rocketfellows\TinkoffInvestV1RestClient\tests\unit;
 
 use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\ClientException as GuzzleClientException;
+use GuzzleHttp\Exception\ServerException as GuzzleServerException;
+use PHPUnit\Framework\MockObject\Builder\InvocationMocker;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 use rocketfellows\TinkoffInvestV1RestClient\Client;
 use rocketfellows\TinkoffInvestV1RestClient\ClientConfig;
+use Throwable;
 
 class ClientTest extends TestCase
 {
@@ -36,5 +42,47 @@ class ClientTest extends TestCase
         $this->httpClient = $this->createMock(HttpClient::class);
 
         $this->client = new Client($this->clientConfig, $this->httpClient);
+    }
+
+    private function assertHttpClientSendRequest(string $uri, array $options, MockObject $response): void
+    {
+        $this->assertHttpClientCallRequest($uri, $options)->willReturn($response);
+    }
+
+    private function assertHttpClientRequestThrowsClientException(string $uri, array $options, string $responseBody): void
+    {
+        $exception = $this->createMock(GuzzleClientException::class);
+        $exception->method('getResponse')->willReturn($this->getResponseMock($responseBody));
+
+        $this->assertHttpClientRequestThrowsException($uri, $options, $exception);
+    }
+
+    private function assertHttpClientRequestThrowsServerException(string $uri, array $options, string $responseBody): void
+    {
+        $exception = $this->createMock(GuzzleServerException::class);
+        $exception->method('getResponse')->willReturn($this->getResponseMock($responseBody));
+
+        $this->assertHttpClientRequestThrowsException($uri, $options, $exception);
+    }
+
+    private function assertHttpClientRequestThrowsException(string $uri, array $options, Throwable $exception): InvocationMocker
+    {
+        return $this->assertHttpClientCallRequest($uri, $options)->willThrowException($exception);
+    }
+
+    private function assertHttpClientCallRequest(string $uri, array $options): InvocationMocker
+    {
+        return $this->httpClient->expects($this->once())->method('request')->with('POST', $uri, $options);
+    }
+
+    private function getResponseMock(string $responseBody): MockObject
+    {
+        $responseBody = $this->createMock(StreamInterface::class);
+        $responseBody->method('getContents')->willReturn($responseBody);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getBody')->willReturn($responseBody);
+
+        return $response;
     }
 }
