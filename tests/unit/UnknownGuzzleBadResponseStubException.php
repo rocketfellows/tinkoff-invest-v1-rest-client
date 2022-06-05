ec91dc2df6ae050e3ca5f15d8c4baf1adba4998c
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-06 00:36:26 +0400

    Added test exception.

---
+++ tests/unit/UnknownGuzzleBadResponseStubException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1RestClient\tests\unit;
+
+use GuzzleHttp\Exception\BadResponseException;
+
+class UnknownGuzzleBadResponseStubException extends BadResponseException
+{
+}
