9a4df9e34ad5aa23ce64546681f9103e71c8317d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-06 01:01:44 +0400

    Covered server exception.

---
+++ tests/unit/exceptions/request/ServerExceptionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1RestClient\tests\unit\exceptions\request;
+
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\BadResponseData;
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\BadResponseException;
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\ServerException;
+use Throwable;
+
+class ServerExceptionTest extends BadResponseExceptionTest
+{
+    public function instantiateException(
+        ?int $badResponseCode,
+        ?string $badResponseMessage,
+        ?string $badResponseDescription,
+        ?string $message,
+        ?int $code,
+        ?Throwable $previousException
+    ): BadResponseException {
+        return new ServerException(
+            (new BadResponseData($badResponseCode, $badResponseMessage, $badResponseDescription)),
+            $message,
+            $code,
+            $previousException
+        );
+    }
+}
